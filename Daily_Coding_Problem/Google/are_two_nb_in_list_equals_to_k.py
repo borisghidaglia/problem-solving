dc7017970004301a8a88a18a1c6fa4b34f4384b7
borisghidaglia <borisghidaglia@gmail.com>
2019-02-23 17:53:58 +0100

    update on a Google DCP

--- Daily_Coding_Problem/Google/are_two_nb_in_list_equals_to_k.py
+++ Daily_Coding_Problem/Google/are_two_nb_in_list_equals_to_k.py
@@ -1,33 +1,30 @@
-# This problem was recently asked by Google.
-#
-# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
-#
-# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-#
-# Bonus: Can you do this in one pass? aka O(n)
+"""This problem was recently asked by Google.
 
-short = [10, 15, 3, 7]
-k = 17
+Given a list of numbers and a number k, return whether any two numbers from the list
+add up to k.
 
-vals = set()
-# O(n)
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass? aka O(n)
+"""
+
 def solve(array, k):
-    # O(n)
+    """ The idea is to use a set.
+    First, we have to create an empty set.
+    Then, for each element in the array, if its complementary value is in the set, then return
+    true. Otherwise, we add the element in the set. 
+    """
+    vals = set()
+
     for elt in array:
-        # O(1)
         if k - elt in vals:
             return True
-        # O(1)
         vals.add(elt)
 
     return False
 
 
-# True
 assert solve([10, 15, 3, 7], 17)
-# False
 assert not solve([10, 15, 3, 4], 17)
-# Empty array case
 assert not solve([], 17)
-# Duplicated vals case
 assert solve([6, 6], 12)
