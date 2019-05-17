9fbf23119fc0a28750995dd06573aa646215241d
borisghidaglia <borisghidaglia@gmail.com>
2019-05-17 10:02:29 +0200

    adding a DCP

---
+++ Daily_Coding_Problem/Airbnb/9_non_adjacent.py
@@ -0,0 +1,69 @@
+"""This problem was asked by Airbnb.
+
+Given a list of integers, write a function that returns
+the largest sum of non-adjacent numbers. Numbers can
+be 0 or negative.
+
+For example, [2, 4, 6, 2, 5] should return 13, since we
+pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since
+we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant
+space?
+"""
+
+# Fully Recursive
+def solve(l):
+    if len(l) <= 2:
+        return max(l)
+    with_last = solve(l[:-2]) + l[-1]
+    without = solve(l[:-1])
+    return max(with_last, without)
+
+# Dynamic Programming
+def solve_linear(l):
+    # if in store
+    if store.get(len(l)): return store.get(len(l))
+    # else
+    if len(l) <= 2:
+        return max(l)
+    with_last = solve_linear(l[:-2]) + l[-1]
+    without = solve_linear(l[:-1])
+    best = max(with_last, without)
+    store[len(l)] = best
+    return best
+
+# Iterative version linear time and constant space
+def solve_linear_it(l):
+    max_before = (None, None)
+    max_bbefore = (None, None)
+    for idx, elt in enumerate(l):
+
+        if not max_before[0]:
+            max_before = (elt, idx)
+            continue
+
+        if not max_bbefore[0]:
+            if elt > max_before[0]:
+                max_before, max_bbefore = (elt, idx), max_before
+                continue
+            else: max_bbefore = max_before; continue
+
+        if elt + max_before[0] > max_before[0] and idx - max_before[1] > 1:
+            max_before, max_bbefore = (max_before[0] + elt, idx), max_before
+        elif elt + max_bbefore[0] > max_before[0]:
+            max_before, max_bbefore = (elt + max_bbefore[0], idx), max_before
+
+    return max_before[0]
+
+
+assert solve([2, 4, 6, 2, 5]) == 13
+assert solve([5, 1, 1, 5]) == 10
+
+store = {}
+assert solve_linear([2, 4, 6, 2, 5]) == 13
+store = {}
+assert solve_linear([5, 1, 1, 5]) == 10
+
+assert solve_linear_it([2, 4, 6, 2, 5]) == 13
+assert solve_linear_it([5, 1, 1, 5]) == 10
--- Daily_Coding_Problem/Readme.md
+++ Daily_Coding_Problem/Readme.md
@@ -1,24 +1,35 @@
 # [Daily Coding Problem](https://www.dailycodingproblem.com/)
+
 Daily Coding Problem is a mailing list for coding interview problems. Get exceptionally good at coding interviews by solving one problem every day.
 
 ## Problem number by company
+
 You have a coding interview incoming with one of those companies ? Check out their
 problem solving questions here !
 
+### Airbnb
+
+[9](./Airbnb/9_non_adjacent.py)
+
 ### Facebook
+
 [7](./Facebook/7_encoded_message.py)
 
 ### Google
+
 [1](./Google/1_are_two_nb_in_list_equals_to_k.py) -
 [3](./Google/3_binary_tree_serialize_deserialize.py) -
 [6](./Google/6_xor_linked_list.py) -
 [8](./Google/8_unival_tree.py)
 
 ### Jane Street
+
 [5](./Jane_Street/5_cons_car_cdr.py)
 
 ### Stripe
+
 [4](./Stripe/4_first_missing_positive_integers.py)
 
 ### Uber
+
 [2](./Uber/2_product_with_arrays.py)
